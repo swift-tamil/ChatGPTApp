4f81c99655422d4eb2d7bfa56af2be8774ddcc99
MohamedGoush <0x1110111@gmail.com>
2023-10-28 05:29:00 +0530

    Initial Commit

---
+++ iOS_ChatGPT/iOS_ChatGPTApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOS_ChatGPTApp.swift
+//  iOS_ChatGPT
+//
+//  Created by python on 28/10/23.
+//
+
+import SwiftUI
+
+@main
+struct iOS_ChatGPTApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ macOS_ChatGPT/macOS_ChatGPTApp.swift
@@ -0,0 +1,17 @@
+//
+//  macOS_ChatGPTApp.swift
+//  macOS_ChatGPT
+//
+//  Created by python on 28/10/23.
+//
+
+import SwiftUI
+
+@main
+struct macOS_ChatGPTApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
